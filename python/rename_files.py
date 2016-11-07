57d6562ccc20e39e9c8452a9ace3408d13df72e2
suisheng hu <suisheng hu>
2016-11-07 09:56:46 +0800

    begin udacity python

---
+++ python/rename_files.py
@@ -0,0 +1,6 @@
+import os
+def rename_files():
+	files_list = os.listdir("C:\Users\goodniuniu\Github\gitskills\python\prank\prank")
+	print (files_list)
+
+rename_files()
---
+++ python/take_break.py
@@ -0,0 +1,8 @@
+import webbrowser,time
+total_break_time=3
+break_time=0
+while (break_time < total_break_time) :
+    print ("the program starts at ",+time.ctime())
+    time.sleep(10)
+    webbrowser.open("http://www.baidu.com")
+    break_time = break_time+1
